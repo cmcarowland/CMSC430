1c8df46b2b4e2eb565192ba829bf12322e9618ae
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-06-10 23:26:48 +0000

    feat: Moved files forward from project 1

--- Project2/src/listing.cc
+++ Project2/src/listing.cc
@@ -1,5 +1,5 @@
 // CMSC 430 Compiler Theory and Design
-// Project 2 Skeleton
+// Project 1 Skeleton
 // UMGC CITE
 // Summer 2023
 
@@ -8,13 +8,15 @@
 
 #include <cstdio>
 #include <string>
+#include <queue>
 
 using namespace std;
 
 #include "listing.h"
 
 static int lineNumber;
-static string error = "";
+static queue<Error*> errors;
+static int errorCounts[] = {0, 0, 0, 0, 0}; // LEXICAL, SYNTAX, GENERAL_SEMANTIC, DUPLICATE_IDENTIFIER, UNDECLARED
 static int totalErrors = 0;
 
 static void displayErrors();
@@ -34,25 +36,58 @@ void nextLine()
 
 int lastLine()
 {
-	printf("\r");
-	displayErrors();
-	printf("     \n");
+	printf("\n\n");
+	if(totalErrors > 0)
+	{
+		printf("%d errors found. Compilation Failed!\n", totalErrors);
+		printf("=========================================================\n");
+		printf("Lexical Errors.             : %d\n", errorCounts[LEXICAL]);
+		printf("Syntactic Errors            : %d\n", errorCounts[SYNTAX]);
+		printf("Semantic Errors.            : %d\n", errorCounts[GENERAL_SEMANTIC]);
+		printf("Duplicate Identifier Errors : %d\n", errorCounts[DUPLICATE_IDENTIFIER]);
+		printf("Undeclared Identifier Errors: %d\n\n", errorCounts[UNDECLARED]);
+		displayErrors();
+	} else
+		printf("Compiled Successfully!!!\n");
+	
 	return totalErrors;
 }
     
 void appendError(ErrorCategories errorCategory, string message)
 {
-	string messages[] = { "Lexical Error, Invalid Character ", "",
-		"Semantic Error, ", "Semantic Error, Duplicate ",
-		"Semantic Error, Undeclared " };
+	Error *error = new Error(lineNumber, errorCategory, message);
+	errors.push(error);
 
-	error = messages[errorCategory] + message;
+	errorCounts[errorCategory]++;
 	totalErrors++;
 }
 
 void displayErrors()
 {
-	if (error != "")
-		printf("%s\n", error.c_str());
-	error = "";
+	while(errors.size() != 0)
+	{
+		Error *e = errors.front();
+		e->Display();
+		errors.pop();
+	}
+}
+
+Error::Error(int lineNumber, ErrorCategories errorCategory, string lineContent)
+	: lineNumber(lineNumber), errorCategory(errorCategory) {
+	this->lineContent = lineContent;
+}
+
+string Error::ErrorEnumToString(ErrorCategories errorCategory) const {
+	switch (errorCategory) {
+		case LEXICAL: return "Lexical Error";
+		case SYNTAX: return "Syntax Error";
+		case GENERAL_SEMANTIC: return "General Semantic Error";
+		case DUPLICATE_IDENTIFIER: return "Duplicate Identifier Error";
+		case UNDECLARED: return "Undeclared Identifier Error";
+		default: return "Unknown Error";
+	}
+}
+
+void Error::Display() const {
+	printf("\t -%s : Invalid Character %s\n", ErrorEnumToString(errorCategory).c_str(), lineContent.c_str());
 }
