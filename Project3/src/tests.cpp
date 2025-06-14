883e211937e667f64b0dbf9c192a9285747f458e
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-06-14 23:41:24 +0000

    test: Updated test build to allow for calling methods and testing in the google test process

--- Project3/src/tests.cpp
+++ Project3/src/tests.cpp
@@ -9,49 +9,32 @@ const string YELLOW = "\033[33m";
 const string LIGHT_BLUE = "\033[94m";
 const string RESET = "\033[0m";
 
+#define TESTING 1
+
+double parse();
+void yyset_in  ( FILE * _in_str  );
 
 namespace {
     class SuppliedTest : public testing::Test {
     protected:
-        const int MAX_LINE = 1024;
-        const string binaryPath = "bin/project2.elf";
-        FILE *pipe;
         FILE *input;
+        double result;
 
         SuppliedTest() {}
 
-        ~SuppliedTest() {
-            if (input) {
-                fclose(input);
-            }
-        }
+        ~SuppliedTest() {}
 
-        int SetUp(string fileName) {
-            pipe = popen(binaryPath.c_str(), "w");
-            if(!pipe)
-            {
-                perror("popen failed");
-                exit(EXIT_FAILURE);
-            }
-
+        void SetUp(string fileName) {
             input = fopen(fileName.c_str(), "r");
             if(!input)
             {
                 perror("fopen failed");
                 exit(EXIT_FAILURE);
             }
-
-            char buffer[MAX_LINE];
-            while(fgets(buffer, sizeof(buffer), input)) {
-                fputs(buffer, pipe);
-            }
-
-            int status = pclose(pipe);
-            if (WIFEXITED(status)) {
-                return WEXITSTATUS(status);
-            }
-
-            return -1;
+            yyset_in(input);
+            result = parse();
+            fclose(input);
+            input = nullptr; 
         }
     };
 
@@ -64,128 +47,128 @@ namespace {
     TEST_F(SuppliedTest, Test1) { 
         string s = "data/test1.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
+        SetUp(s);
+        EXPECT_EQ(25, result) << "Failed!! " << s;
         cout << result_header(s, result);
     }
 
     TEST_F(SuppliedTest, Test2) { 
         string s = "data/test2.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
+        SetUp(s);
+        EXPECT_EQ(9, result) << "Failed!! " << s;
         cout << result_header(s, result);
     }
     
     TEST_F(SuppliedTest, Test3) { 
         string s = "data/test3.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
+        SetUp(s);
+        EXPECT_EQ(99, result) << "Failed!! " << s;
         cout << result_header(s, result);
     }
     
     TEST_F(SuppliedTest, Test4) { 
         string s = "data/test4.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
+        SetUp(s);
+        EXPECT_EQ(8, result) << "Failed!! " << s;
         cout << result_header(s, result);
     }
     
     TEST_F(SuppliedTest, Test5) { 
         string s = "data/test5.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
+        SetUp(s);
+        EXPECT_EQ(115.57, result) << "Failed!! " << s;
         cout << result_header(s, result);
     }
    
     TEST_F(SuppliedTest, Test6) { 
         string s = "data/test6.txt";
        
-        double result = SetUp(s);
+        SetUp(s);
         EXPECT_EQ(0, result) << "Failed!! " << s;
         cout << result_header(s, result);
     }
    
-    TEST_F(SuppliedTest, Test7) { 
-        string s = "data/test7.txt";
+    // TEST_F(SuppliedTest, Test7) { 
+    //     string s = "data/test7.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
    
-    TEST_F(SuppliedTest, Test8) { 
-        string s = "data/test8.txt";
+    // TEST_F(SuppliedTest, Test8) { 
+    //     string s = "data/test8.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test9) { 
-        string s = "data/test9.txt";
+    // TEST_F(SuppliedTest, Test9) { 
+    //     string s = "data/test9.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test10) { 
-        string s = "data/test10.txt";
+    // TEST_F(SuppliedTest, Test10) { 
+    //     string s = "data/test10.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test11) { 
-        string s = "data/test11.txt";
+    // TEST_F(SuppliedTest, Test11) { 
+    //     string s = "data/test11.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test12) { 
-        string s = "data/test12.txt";
+    // TEST_F(SuppliedTest, Test12) { 
+    //     string s = "data/test12.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test13) { 
-        string s = "data/test13.txt";
+    // TEST_F(SuppliedTest, Test13) { 
+    //     string s = "data/test13.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test14) { 
-        string s = "data/test14.txt";
+    // TEST_F(SuppliedTest, Test14) { 
+    //     string s = "data/test14.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(0, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(0, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test15) { 
-        string s = "data/test15.txt";
+    // TEST_F(SuppliedTest, Test15) { 
+    //     string s = "data/test15.txt";
        
-        double result = SetUp(s);
-        EXPECT_EQ(1, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    //     SetUp(s);
+    //     EXPECT_EQ(1, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 
-    TEST_F(SuppliedTest, Test16) { 
-        string s = "data/test16.txt";
-       
-        double result = SetUp(s);
-        EXPECT_EQ(1, result) << "Failed!! " << s;
-        cout << result_header(s, result);
-    }
+    // TEST_F(SuppliedTest, Test16) { 
+    //     string s = "data/test16.txt";
+
+    //     SetUp(s);
+    //     EXPECT_EQ(1, result) << "Failed!! " << s;
+    //     cout << result_header(s, result);
+    // }
 }
