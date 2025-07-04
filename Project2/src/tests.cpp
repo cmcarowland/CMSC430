1a3a3ec03694e90dd43add38acc23249e5cc744e
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-07-04 14:03:28 +0000

    refactor: Updated color reset to match google test's definition

--- Project2/src/tests.cpp
+++ Project2/src/tests.cpp
@@ -26,7 +26,7 @@ const string RED = "\033[31m";
 const string GREEN = "\033[32m";
 const string YELLOW = "\033[33m";
 const string LIGHT_BLUE = "\033[94m";
-const string RESET = "\033[0m";
+const string RESET = "\033[m";
 
 extern double parse();
 extern void yyset_in  ( FILE * _in_str  );
