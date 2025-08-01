e6e5d9a0cc6d01667d5c77949704c458b3c133f1
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2025-08-01 20:18:09 +0000

    refactor: Modified error output to match guide

--- Project4/src/error.cpp
+++ Project4/src/error.cpp
@@ -11,7 +11,7 @@ string Error::ErrorEnumToString(ErrorCategories errorCategory) const {
 	switch (errorCategory) {
 		case LEXICAL: return "Lexical Error";
 		case SYNTAX: return "Syntax Error";
-		case GENERAL_SEMANTIC: return "General Semantic Error";
+		case GENERAL_SEMANTIC: return "Semantic Error";
 		case DUPLICATE_IDENTIFIER: return "Duplicate Identifier Error";
 		case UNDECLARED: return "Undeclared Identifier Error";
 		default: return "Unknown Error";
